04f38e503f88325d6169397c92f56d3c450755f2
default <61233505+dev-cetus@users.noreply.github.com>
2024-07-29 09:37:46 +0000

    fix: move pages to /admin

--- app/pages/admin/home.vue
+++ app/pages/admin/home.vue
@@ -6,8 +6,8 @@ useSeoMeta({
 
 <template>
     <div>
-        <div class="mx-auto mt-24 flex md:w-1/2 flex-col items-center space-y-4">
-            <h1 class="sm:text-6xl text-4xl font-bold">SPLRGE Go: Link less, share more.</h1>
+        <div class="mx-auto mt-24 flex flex-col items-center space-y-4 md:w-1/2">
+            <h1 class="text-4xl font-bold sm:text-6xl">SPLRGE Go: Link less, share more.</h1>
             <span class="text-gray-200">Open source on <NuxtLink
 class="text-blue-500 hover:underline" to="https://github.com/splrge/go"
                     :external="true">Github</NuxtLink></span>
